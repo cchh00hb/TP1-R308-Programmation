a0c2b8a0114f085d7843660c8ae900208746895e
chfhm <chahinez.fahmi@gmail.com>
2025-09-15 22:41:41 +0200

    jusqua classe cerle, mais avec erreurs

--- classCercle.py
+++ classCercle.py
@@ -1,6 +1,6 @@
 import math
 from typing import Self
-
+from Point import Point
 
 class Cercle :
     def __init__(self,rayon:float,centre: Point = none):
@@ -9,3 +9,25 @@ class Cercle :
             self.__centre = centre
         else :
             self.__centre = Point(0.0)
+
+    @property
+    def rayon(self) -> float :
+        return self.__rayon
+
+    @rayon.setter
+    def rayon (self, rayon : float):
+        self.__rayon = rayon,
+
+    @property
+    def centre (self) -> Point :
+        return self.__centre
+
+    @centre.setter
+    def centre(self,centre:Point):
+        self.__centre=centre
+
+    def perimetre (self)->float:
+        return self.__rayon*2
+
+    def perimetre(self)-> float:
+        return self.__rayon*2*math.py
